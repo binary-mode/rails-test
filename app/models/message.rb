92fc3050c039b33d34c7f8b4d46c483a3fa220f4
emekafredy <chinedufrderick@gmail.com>
2020-11-19 13:26:33 +0100

    fix - arrange messages by date, in descending order.

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ApplicationRecord
   validates :name, :entry, :ip_address, presence: true
 
   scope :by_ip_address, ->(ip) { where(ip_address: ip) }
+
+  default_scope { order(created_at: :desc) }
 end
